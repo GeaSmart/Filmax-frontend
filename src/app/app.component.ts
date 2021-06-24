acf3525a0cdd19a6f90fe4d1dc468b00bce6e912
Gerson Azabache <gerson_dunk@hotmail.com>
2021-06-24 17:37:17 -0500

    cambios añadidos de practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-end';
+  title = 'front-ends';
 
   titulo:string = "d";
 
   body='Gerson Azabache';
 
+  pelicula = {
+    titulo:'Spider man',
+    fechaLanzamiento: new Date(),
+    presupuesto: 1500000
+  };
+
+  listadoPeliculas = [
+    {
+      titulo:'Moana',
+      sinopsis: 'Pelicula pa chibolos sanos',
+      presupuesto: 433554.99    
+    },
+    {
+      titulo:'Rocky',
+      sinopsis: 'Pelicula pa machos pelos en el pecho',
+      presupuesto: 1500000
+    },
+    {
+      titulo:'Gol',
+      sinopsis: 'Pelicula pa peloteros entusiastas',
+      presupuesto: 500000
+    },
+    {
+      titulo:'Anaconda brava',
+      sinopsis: 'Pelicula pa los que le gusta la acción',
+      presupuesto: 780000.50
+    },
+  ];
+
   manejarClick(){
     alert("heyy");
 
-    let listado = this.calcularSuma();
+    let listado = this.calcularSuma(4,5);    
     
-    listado.forEach(element => {
-      alert(element.toString());      
-    });
+    // listado.forEach(element => {
+    //   alert(element.toString());      
+    // });
   }
 
-  calcularSuma(){
-    return [2,3,5];    
+  calcularSuma(a:number, b:number){
+    return a+b;
   }
 }
