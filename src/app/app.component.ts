4aae00c2b55719f822ac8690d885dd20cbcfff47
Gerson Azabache <gerson_dunk@hotmail.com>
2021-06-24 16:39:58 -0500

    testing variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'front-end';
 
+  titulo:string = "d";
+
+  body='Gerson Azabache';
+
   manejarClick(){
-    //hacer lo que yo quiera
+    alert("heyy");
+
+    let listado = this.calcularSuma();
+    
+    listado.forEach(element => {
+      alert(element.toString());      
+    });
+  }
+
+  calcularSuma(){
+    return [2,3,5];    
   }
 }
