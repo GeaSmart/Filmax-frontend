b673f77e726c6a81dcff0f7f316ca4623a17db6d
Gerson Azabache <gerson_dunk@hotmail.com>
2021-06-25 18:13:01 -0500

    security point

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -15,26 +15,31 @@ export class ListadoPeliculasComponent implements OnInit {
         titulo: 'Moana',
         sinopsis: 'Pelicula pa chibolos sanoss',
         presupuesto: 433554.99,
+        fechaEstreno: new Date('01/05/2000')
       },
       {
         titulo: 'Rocky',
         sinopsis: 'Pelicula pa machos pelos en el pecho',
         presupuesto: 1500000,
+        fechaEstreno: new Date('02/05/2000')
       },
       {
         titulo: 'Gol',
         sinopsis: 'Pelicula pa peloteros entusiastas',
         presupuesto: 500000,
+        fechaEstreno: new Date('01/05/2022')
       },
       {
         titulo: 'Anaconda brava',
         sinopsis: 'Pelicula pa los que le gusta la acción',
         presupuesto: 780000.5,
+        fechaEstreno: new Date('01/08/2021')
       },
       {
         titulo: 'Anaconda 2',
         sinopsis: 'Pelicula pa los que le gusta las malas pelis',
         presupuesto: 8621.5,
+        fechaEstreno: new Date('01/05/2010')
       },
     ];
   }
