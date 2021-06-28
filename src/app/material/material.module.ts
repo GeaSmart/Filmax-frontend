6ac21b12c7a8454ecac42d4a297b6877ab6b90c8
Gerson Azabache <gerson_dunk@hotmail.com>
2021-06-28 18:31:08 -0500

    secure point

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 
 @NgModule({
   declarations: [],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   imports: [
     CommonModule,    
