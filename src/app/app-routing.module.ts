5f3f4b28959f0bf9e862b15b52ebe50c8e15b5b4
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-14 16:33:10 -0500

    first routing applied

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LandingPageComponent
+  },
+  {
+    path:'generos',
+    component:IndiceGenerosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
