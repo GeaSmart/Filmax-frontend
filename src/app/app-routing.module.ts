49fa24e8743cd4eb5f4de93a7fe891cb9d5c2ad8
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-14 17:34:42 -0500

    components created and creating routes between them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearActorComponent } from './actores/crear-actor/crear-actor.component';
 import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
+import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
+import { IndiceCinesComponent } from './cines/indice-cines/indice-cines.component';
 import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
 import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
+import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 
 const routes: Routes = [
   {
@@ -15,13 +20,32 @@ const routes: Routes = [
     component:IndiceGenerosComponent
   },
   {
+    path:'generos/crear',
+    component:CrearGeneroComponent
+  },
+
+  {
     path:'actores',
     component:IndiceActoresComponent
   },
   {
-    path:'generos/crear',
-    component:CrearGeneroComponent
-  }
+    path:'actores/crear',
+    component:CrearActorComponent
+  },
+  {
+    path:'cines',
+    component:IndiceCinesComponent
+  },
+  {
+    path:'cines/crear',
+    component:CrearCineComponent
+  },
+
+  //de indice peliculas no lo creamos porque usaremos el de filtro-peliculas
+  {
+    path:'peliculas/crear',
+    component:CrearPeliculaComponent
+  },
 ];
 
 @NgModule({
