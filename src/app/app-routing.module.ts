dc6b948573d115a8d7fde2adbbc9b145136bf76f
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-15 15:50:29 -0500

    routing configuring with paramd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,21 @@ import { CrearActorComponent } from './actores/crear-actor/crear-actor.component
 import { EditarActorComponent } from './actores/editar-actor/editar-actor.component';
 import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
 import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
+import { EditarCineComponent } from './cines/editar-cine/editar-cine.component';
 import { IndiceCinesComponent } from './cines/indice-cines/indice-cines.component';
 import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
+import { EditarGenerosComponent } from './generos/editar-generos/editar-generos.component';
 import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
+import { EditarPeliculaComponent } from './peliculas/editar-pelicula/editar-pelicula.component';
 import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'generos', component: IndiceGenerosComponent},
   {path: 'generos/crear',component: CrearGeneroComponent},
+  {path: 'generos/editar/:id',component: EditarGenerosComponent},
 
   {path: 'actores',component: IndiceActoresComponent },
   {path: 'actores/crear', component: CrearActorComponent},
@@ -22,9 +26,11 @@ const routes: Routes = [
   
   {path: 'cines',component: IndiceCinesComponent},
   {path: 'cines/crear',component: CrearCineComponent},
+  {path: 'cines/editar/:id',component: EditarCineComponent},
   
   //de indice peliculas no lo creamos porque usaremos el de filtro-peliculas
   {path: 'peliculas/crear',component: CrearPeliculaComponent},
+  {path: 'peliculas/editar/:id',component: EditarPeliculaComponent},
 
 ];
 
